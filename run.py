171129fba1cb1655a28081176f7993e223c8c66e
kevinyanAtSsense <kevin.yan@ssense.com>
2022-12-30 14:50:35 -0500

    cleanup

--- run.py
+++ run.py
@@ -5,8 +5,8 @@ from booking.book import Booking
 
 with Booking() as bot:
     bot.land_first_page()
-    bot.user_name('kevinyan13')
-    bot.pass_word('Kejiji@94629')
+    bot.user_name('')
+    bot.pass_word('')
     # bot.select_month()
     bot.select_date(date = "2022-12-29")
     bot.select_time(time_court = "1800|1900|2")
