ed39c3f8cd08bcd5dd248b4bc969759520988dad
kevinyanAtSsense <kevin.yan@ssense.com>
2022-12-30 16:30:20 -0500

    add security

--- booking/book.py
+++ booking/book.py
@@ -1,11 +1,13 @@
 import os
+import time
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+import datetime
 
-
+import env.const as const
 
 class Booking(webdriver.Chrome):
-    def __init__(self, driver_path=r"C:\SeleniumDrivers",
+    def __init__(self, driver_path=r"/Users/kevin.yan/Downloads",
                  teardown=False):
         self.driver_path = driver_path
         self.teardown = teardown
@@ -21,21 +23,29 @@ class Booking(webdriver.Chrome):
     def land_first_page(self):
         self.get("https://members.swtc.ca/")
 
-    def user_name(self, user_name):
+    def get_password(self):
+        password = const.password
+        return password
+    
+    def get_username(self):
+        username = const.username
+        return username
+
+    def set_username(self, user_name):
         user_name_element = self.find_element(By.ID, 'login-form-username')
         user_name_element.clear()
         user_name_element.send_keys(user_name)
 
-    def pass_word(self, pass_word):
+    def set_password(self, pass_word):
         pass_word_element = self.find_element(By.ID, 'login-form-password')
         pass_word_element.clear()
         pass_word_element.send_keys(pass_word)
         log_in = self.find_element(By.ID, 'login-form-submit')
         log_in.click()
-
-    # def select_month(self):
-    #     right = self.find_element(By.CSS_SELECTOR, 'i[class="i-rounded i-small i-light i-alt icon-chevron-right"]')
-    #     right.click()
+    
+    def select_month(self):
+        right = self.find_element(By.CSS_SELECTOR, 'i[class="i-rounded i-small i-light i-alt icon-chevron-right"]')
+        right.click()
 
     def select_date(self,date):
         date_element = self.find_element(By.CSS_SELECTOR, f'button[data-value="{date}"]')
@@ -46,10 +56,16 @@ class Booking(webdriver.Chrome):
         time_element.click()
 
     def book_submit(self):
-        submit_booking = self.find_element(By.CSS_SELECTOR, 'a[onclick="book()"]')
+        submit_booking = self.find_element(By.ID, 'book-button2')
         submit_booking.click()
         confirm_booking = self.find_element(By.CSS_SELECTOR, 'a[onclick="bookSubmit()"]')
         confirm_booking.click()
+    
+    def next_month(self):
+        target_date = datetime.datetime.now() + datetime.timedelta(days=6)
+        target_date_month = target_date.strftime('%Y-%m')
+        current_month = datetime.datetime.now().strftime('%Y-%m')
+        return target_date_month != current_month
 
 
 
--- run.py
+++ run.py
@@ -1,14 +1,17 @@
 import time
-
+import datetime
 from booking.book import Booking
 
+target_date = datetime.datetime.now() + datetime.timedelta(days=6)
+target_date_formatted = target_date.strftime('%Y-%m-%d')
 
 with Booking() as bot:
     bot.land_first_page()
-    bot.user_name('')
-    bot.pass_word('')
-    # bot.select_month()
-    bot.select_date(date = "2022-12-29")
-    bot.select_time(time_court = "1800|1900|2")
+    bot.set_username(bot.get_username())
+    bot.set_password(bot.get_password())
+    if (bot.next_month()):
+        bot.select_month()
+    bot.select_date(date = target_date_formatted)
+    bot.select_time(time_court = "2200|2300|2")
     bot.book_submit()
-    time.sleep(5)
+    time.sleep(150000)
